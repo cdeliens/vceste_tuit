57d67145ca623affe60c04e0372a0b3fec021ed5
cdeliens <cdeliens@gmail.com>
2012-03-01 22:06:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VcesteTuit::Application.config.secret_token = 'd85fa2064da49473a5c1206621d48a9b96b94f643667fd66297f83a59e99767f14ccbd71ac0d1a23be6990c105320bff5f2c6a6b4402821e76a8274c3a7b0693'
