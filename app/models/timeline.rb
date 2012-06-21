47f98ae48518428cfcb65e44499046a8d9abc85d
cdeliens <cdeliens@gmail.com>
2012-06-21 11:55:57 -0600

    pretty urls

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,4 +2,8 @@ class Timeline < ActiveRecord::Base
   validates :hashtag,
   :presence   => true,
   :format     => { :with => /#\S*/ }
+
+  def to_param
+    "#{hashtag.gsub("#", "")}".parameterize
+  end
 end
